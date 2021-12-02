b7b77abe7446754f48b188bfdcabde45bd51d371
sireeshabondada <91303755+sireeshabondada@users.noreply.github.com>
2021-12-02 19:26:58 +1400

    Add files via upload

---
+++ Evennumbers.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Hi
+ */
+public class Evennumbers 
+{
+    public static void main(String args[])
+    {
+        int n=22;  
+        if(n%2==0)
+        {
+            System.out.println("even number");
+        }
+        else
+        {
+            System.out.println("not a even number");
+        }
+    }
+}
